24c9c837638d65fac78d18795b825b07c3a9f724
Ivan Kirshak <i.kirshak@gmail.com>
2020-02-04 17:47:55 +0200

    added functionality

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 let answerBtn = document.getElementById("answerBtn");
 let answer = document.getElementsByName("answer");
 let results = document.getElementById("results");
+let anotherq = document.getElementById("anotherq");
+let newQuestion = document.getElementById("newQuestion");
+let newResult = document.getElementById("newResult");
+
+function check() {
+    let correct = document.getElementsByClassName("correct");
+    if (correct.checked) {
+        newResult.innerHTML = `<h1 class="true">Yes, you're right!. <br>
+        To try again, click "Give me another question" button above</h1>`;
+    } else {
+        newResult.innerHTML = `<h1 class="false">Sorry, not true. <br>
+        To try again, click "Give me another question" button above</h1>`;
+    }
+}
+
+function giveQuestion() {
+    const XHR = new XMLHttpRequest();
+    let URI = `https://opentdb.com/api.php?amount=1&type=boolean`;
+    XHR.addEventListener("readystatechange", function () {
+        if (XHR.status === 200 && XHR.readyState === 4) {
+            let result = JSON.parse(XHR.responseText);
+            //console.log(result);
+            newQuestion.innerHTML = `
+            <h1 class="question">${result.results[0].question}?</h1>
+            <div class="answers">
+            <label><input name="question" type="radio" class="correct"> ${result.results[0].correct_answer}</label>
+            <label><input name="question" type="radio" > ${result.results[0].incorrect_answers[0]}</label>
+            </div>
+            <button onclick="check()" class="answerBtn">Submit</button>
+            `;
+        }
+    }, false);
+    XHR.open("GET", URI);
+    XHR.send();
+}
 
 function lookForAnswer() {
     for (let i = 0; i < answer.length; i++) {
@@ -10,5 +45,11 @@ function lookForAnswer() {
             results.innerHTML = `<h1 class="false">Sorry, not true.</h1>`
         }
     }
+    //creation of button for another question 
+    let moreq = document.createElement("button");
+    moreq.innerText = "Give me another question";
+    moreq.classList.add("answerBtn");
+    anotherq.appendChild(moreq);
+    moreq.addEventListener("click", giveQuestion, false);
 }
 answerBtn.addEventListener("click", lookForAnswer, false);
